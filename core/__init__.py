d8e2544a6eaf1bc8e42a26982b577d45e3fa7327
Chad Holland <261277573+holland202@users.noreply.github.com>
2026-07-14 18:52:53 -0500

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,8 @@
-[paste __init__.py content]
+"""SLC v12 Core Package - Chad Edward Holland
+Sovereign Logic Core - Unified Manifold Architecture
+Vincit Omnia Veritas
+"""
+
+__version__ = "12.0.0"
+__author__ = "Chad Edward Holland"
+__license__ = "MIT"
